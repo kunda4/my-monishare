b4268fbd8711f1f44349003e883d024c44c366ce
Agnes Iradukunda <kundaaggy@gmail.com>
2024-01-08 19:32:15 +0200

    feat: creating booking entity

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,6 +1,9 @@
+import { from } from 'rxjs'
+
 export * from './access-denied.error'
 export * from './authentication'
 export * from './car'
 export * from './car-type'
 export * from './time-provider.interface'
 export * from './user'
+export * from './booking'
---
+++ src/persistence/booking.repository.ts
@@ -0,0 +1,2 @@
+import { Injectable } from "@nestjs/common";
+import {type Except} from 'type-fest'
