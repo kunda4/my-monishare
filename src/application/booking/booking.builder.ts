a77b1bbd3f490c897632e12f0e9ced446bf9640b
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-09 16:37:25 +0200

    feat: create booking builder

---
+++ src/application/booking/booking.builder.ts
@@ -0,0 +1,89 @@
+import { type Except } from 'type-fest'
+
+import { Booking, type BookingID, type BookingProperties } from '../booking'
+import { type Car, type CarID } from '../car'
+import { type User, type UserID } from '../user'
+
+import { BookingState } from './booking-state'
+
+type UntaggedBookingProperties = Except<
+  BookingProperties,
+  'id' | 'carId' | 'renterId'
+> & { id: number; carId: number; renterId: number }
+
+export class BookingBuilder {
+  private readonly properties: UntaggedBookingProperties = {
+    id: 10 as BookingID,
+    renterId: 2 as UserID,
+    carId: 1 as CarID,
+    startDate: new Date('2024-02-08T14:07:27.828Z'),
+    endDate: new Date('2024-06-09T07:20:56.959Z'),
+    state: BookingState.PENDING,
+  }
+
+  public static from(
+    properties: Booking | Partial<UntaggedBookingProperties>,
+  ): BookingBuilder {
+    return new BookingBuilder().with(properties)
+  }
+
+  public with(properties: Partial<UntaggedBookingProperties>): this {
+    let key: keyof UntaggedBookingProperties
+
+    for (key in properties) {
+      const value = properties[key]
+
+      if (value !== undefined) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        this.properties[key] = value
+      }
+    }
+
+    return this
+  }
+
+  public withId(id: number): this {
+    this.properties.id = id
+    return this
+  }
+
+  public withRenter(data: number | User): this {
+    this.properties.renterId = typeof data === 'number' ? data : data.id
+
+    return this
+  }
+
+  public withCar(data: number | Car): this {
+    this.properties.carId = typeof data === 'number' ? data : data.id
+
+    return this
+  }
+
+  public withState(data: BookingState): this {
+    this.properties.state = data
+
+    return this
+  }
+
+  public withStartDate(data: Date): this {
+    this.properties.startDate = data
+
+    return this
+  }
+
+  public withEndDate(data: Date): this {
+    this.properties.endDate = data
+
+    return this
+  }
+
+  public build(): Booking {
+    return new Booking({
+      ...this.properties,
+      id: this.properties.id as BookingID,
+      renterId: this.properties.renterId as UserID,
+      carId: this.properties.carId as CarID,
+    })
+  }
+}
