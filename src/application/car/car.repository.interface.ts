64671e14a24f585e26eadf6c032a0a1f23ca5f1c
Agnes Iradukunda <kundaaggy@gmail.com>
2024-01-24 23:28:54 +0200

    ICarRepostory

---
+++ src/application/car/car.repository.interface.ts
@@ -0,0 +1,18 @@
+import { Except } from 'type-fest'
+
+import { type Transaction } from '../../persistence'
+
+import { type Car, type CarID, CarProperties } from './car'
+
+export abstract class ICarRepository {
+  public abstract get(tx: Transaction, id: CarID): Promise<Car>
+
+  public abstract getAll(tx: Transaction): Promise<Car[]>
+
+  public abstract update(tx: Transaction, id: CarID): Promise<Car>
+
+  public abstract insert(
+    tx: Transaction,
+    properties: Except<CarProperties, 'id'>,
+  ): Promise<Car>
+}
