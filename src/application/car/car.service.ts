18e7e50e0c18c8f0abf64fd63a9b0e8996cfe561
Agnes Iradukunda <kundaaggy@gmail.com>
2024-01-25 15:48:16 +0200

    create services for the get method

--- src/application/car/car.service.ts
+++ src/application/car/car.service.ts
@@ -1,7 +1,33 @@
-import {Injectable, Logger} from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { Except } from 'type-fest'
 
-import { IDatabaseConnection } from '../../persistence'
+import {
+  CarRepository,
+  DatabaseConnection,
+  IDatabaseConnection,
+} from '../../persistence'
 
-import {Car, type CarID, CarProperties} from './car'
+import { Car, type CarID, CarProperties } from './car'
 import { ICarRepository } from './car.repository.interface'
+import { ICarService } from './car.service.interface'
+
+export class CarService implements ICarService {
+  private readonly carRepository: ICarRepository
+  private readonly databaseConnection: IDatabaseConnection
+  private readonly logger: Logger
+
+  public constructor(
+    CarRepository: ICarRepository,
+    DatabaseConnection: IDatabaseConnection,
+  ) {
+    this.carRepository = CarRepository
+    this.databaseConnection = DatabaseConnection
+    this.logger = new Logger(CarService.name)
+  }
+
+  public async get(id: CarID): Promise<Car> {
+    return this.databaseConnection.transactional(tx =>
+      this.carRepository.get(tx, id),
+    )
+  }
+}
