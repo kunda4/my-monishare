bbb3e01efa9721d988c7f43875d668b0c0e27773
Agnes Iradukunda <kundaaggy@gmail.com>
2024-01-26 12:25:17 +0200

    add get one method in controller

--- src/controller/car/car.controller.ts
+++ src/controller/car/car.controller.ts
@@ -1,4 +1,27 @@
-import{Car, type CarID, ICarService} from '../../application'
-import { AuthenticationGuard } from '../authentication.guard'
+import { Get, Param, ParseIntPipe } from '@nestjs/common'
 
-import{}
+import {
+  Car,
+  type CarID,
+  ICarService,
+  IUserService,
+  UserID,
+} from '../../application'
+import { AuthenticationGuard } from '../authentication.guard'
+import { UserDTO } from '../user'
+
+import { CarDTO } from './car.dto'
+
+export class CarController {
+  private readonly carService: ICarService
+
+  public constructor(carService: ICarService) {
+    this.carService = carService
+  }
+
+  @Get(':id')
+  public async get(@Param('id', ParseIntPipe) id: CarID): Promise<CarDTO> {
+    const car = await this.carService.get(id)
+    return CarDTO.fromModel(car)
+  }
+}
