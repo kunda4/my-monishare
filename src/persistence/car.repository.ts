cc105eace410c0f35a95781cb6da74414cc21746
Agnes Iradukunda <kundaaggy@gmail.com>
2024-01-29 17:34:28 +0200

    add update method in car repository

--- src/persistence/car.repository.ts
+++ src/persistence/car.repository.ts
@@ -110,4 +110,24 @@ export class CarRepository implements ICarRepository {
     )
     return rowToDomain(row)
   }
+  public async update(tx: Transaction, car: Car): Promise<Car> {
+    const row = await tx.oneOrNone<Row>(
+      `UPDATE cars SET
+      car_type_id = $(carTypeId)
+      owner_id = $(ownerId),
+      name = $(name),
+      state = $(state),
+      fuel_type = $(fuelType),
+      horsepower = $(horsepower),
+      license_plate = $(licensePlate),
+      info = $(info)
+      WHERE id = $(id)
+      RETURNING *`,
+      { ...car },
+    )
+    if (row === null) {
+      throw new CarNotFoundError(car.id)
+    }
+    return rowToDomain(row)
+  }
 }
